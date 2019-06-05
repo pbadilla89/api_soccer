138f7e2ab9570c2112300786a2395d07da4117c3
Pablo Badilla <pbadilla@andbytes.com>
2019-06-05 06:25:35 -0400

    se agrega matches en controlador y modelo

--- api/controllers/matches.js
+++ api/controllers/matches.js
@@ -50,7 +50,38 @@ function playMatch(req, res){
 
   console.log(match)
 
-  res.send({status: true, mensaje: "Se Guardo Correctamente"});
+  const match2 = {
+    name: match.name,
+    win: match.win,
+    idHome: match.idHome._id,
+    idAway: match.idAway._id,
+    league: match.league._id
+  }
+
+  Match.findByIdAndUpdate(match._id, match2, (err, mth) => {
+    if ( err2 ) res.send({status: false, mensaje: "Se genero un error"});
+
+    res.send({status: true, mensaje: "Se Guardo Correctamente"});
+
+    const newHome = {
+      pj: match.win === match.idHome._id ? match.idHome.pj+1 : match.idHome.pj,
+      pe: match.win === "0" ? match.idHome.pe+1 : match.idHome.pe,
+      pg: match.win === match.idHome._id ? match.idHome.pg+1 : match.idHome.pg,
+      pp: match.win === match.idAway._id ? match.idHome.pp+1 : match.idHome.pp,
+      pts: match.win === match.idHome._id ? match.idHome.pts+3 : match.win === "0" ? match.idHome.pts+1 : match.idHome.pts,
+    }
+    Team.findByIdAndUpdate(match.idHome._id,{ ...match.idHome, ...newHome }, (err, userUpload) => {})
+
+    const newAway = {
+      pj: match.win === match.idAway._id ? match.idAway.pj+1 : match.idAway.pj,
+      pe: match.win === "0" ? match.idAway.pe+1 : match.idAway.pe,
+      pg: match.win === match.idAway._id ? match.idAway.pg+1 : match.idAway.pg,
+      pp: match.win === match.idHome._id ? match.idAway.pp+1 : match.idAway.pp,
+      pts: match.win === match.idAway._id ? match.idAway.pts+3 : match.win === "0" ? match.idAway.pts+1 : match.idAway.pts,
+    }
+    Team.findByIdAndUpdate(match.idAway._id,{ ...match.idAway, ...newAway }, (err, userUpload) => {})
+  } )
+  
 }
 
 
