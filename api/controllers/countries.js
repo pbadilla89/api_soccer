a0d8952c0cc97a11b13b28e87e072833cc0221d8
Pablo Badilla <pbadilla@andbytes.com>
2019-06-10 00:09:29 -0400

    se agrega el editar pais en la ruta y el metodo del controlador

--- api/controllers/countries.js
+++ api/controllers/countries.js
@@ -14,8 +14,6 @@ function list(req, res) {
 function save(req, res) {
   var params = req.body
 
-  console.log(params)
-
   if( typeof params._id != "undefined" ){
     const update = {
       name: params.name
